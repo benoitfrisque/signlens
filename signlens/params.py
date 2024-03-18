5554f5f3573276c41aa842716b7b568ec2ac2d8b
Benoit Frisque <benoitfrisque@gmail.com>
2024-03-18 18:45:01 +0100

    implemented preprocess_files.py to reduce file size by filtering face landmarks

--- signlens/params.py
+++ signlens/params.py
@@ -5,7 +5,7 @@ import numpy as np
 
 ##################  CONSTANTS  #####################
 
-TRAIN_DATA_DIR   = os.path.join('..', 'raw_data/asl-signs')
+TRAIN_DATA_DIR   = os.path.join('..', '..', 'raw_data', 'asl-signs')
 TRAIN_CSV_PATH   = os.path.join(TRAIN_DATA_DIR, 'train.csv')
 LANDMARK_DIR     = os.path.join(TRAIN_DATA_DIR,'train_landmark_files')
 LABEL_MAP_PATH   = os.path.join(TRAIN_DATA_DIR,'sign_to_prediction_index_map.json')
