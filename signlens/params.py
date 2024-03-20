b6f2e413f662d8ed08abe6935734172da2c03c19
Benoit Frisque <benoitfrisque@gmail.com>
2024-03-20 13:00:22 +0100

    improved preprocesses_files_light, cleaned data.py, implemented global path in params.py

--- signlens/params.py
+++ signlens/params.py
@@ -5,13 +5,15 @@ import numpy as np
 
 ##################  CONSTANTS  #####################
 
-TRAIN_DATA_DIR   = os.path.join('..', '..', 'raw_data', 'asl-signs')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__)) # go 2 levels up
+
+TRAIN_DATA_DIR   = os.path.join(BASE_DIR, 'raw_data', 'asl-signs')
 TRAIN_CSV_PATH   = os.path.join(TRAIN_DATA_DIR, 'train.csv')
 LANDMARK_DIR     = os.path.join(TRAIN_DATA_DIR,'train_landmark_files')
 LABEL_MAP_PATH   = os.path.join(TRAIN_DATA_DIR,'sign_to_prediction_index_map.json')
 
-N_LANDMARKS_HAND = 21
-N_LANDMAKRS_POSE = 33
-N_LANDMARKS_FACE = 468
-N_LANDMARKS = N_LANDMARKS_HAND * 2 +  N_LANDMAKRS_POSE + N_LANDMARKS_FACE
-N_LANDMARKS_NO_FACE = N_LANDMAKRS_POSE + 2 * N_LANDMARKS_HAND
+N_LANDMARKS_HAND    = 21
+N_LANDMAKRS_POSE    = 33
+N_LANDMARKS_FACE    = 468
+N_LANDMARKS_ALL     = 543   # N_LANDMARKS_HAND * 2 +  N_LANDMAKRS_POSE + N_LANDMARKS_FACE
+N_LANDMARKS_NO_FACE = 75    # N_LANDMAKRS_POSE + 2 * N_LANDMARKS_HAND
