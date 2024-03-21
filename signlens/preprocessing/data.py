16ae24420bfed10567a106e803ba6a488468cb38
Benoit Frisque <benoitfrisque@gmail.com>
2024-03-21 14:03:51 +0100

    improved loading functions to see what has been loaded

--- signlens/preprocessing/data.py
+++ signlens/preprocessing/data.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from termios import N_SLIP
 import pandas as pd
 import numpy as np
 import math
@@ -17,146 +18,193 @@ import random
 # LOAD CSV
 ################################################################################
 
-def load_data_subset_csv(frac=DATA_FRAC, noface=True, balanced=False, num_signs=NUM_CLASSES):
+def load_data_subset_csv(frac=DATA_FRAC, noface=True, balanced=False, n_classes=NUM_CLASSES, n_frames=MAX_SEQ_LEN):
     '''
-    Load a data subset, as a fraction of the original dataset. It can be balanced, and the number of signs can be limited.
+    Load a data subset, as a fraction of the original dataset. It can be balanced, and the number of classes can be limited.
 
     Parameters:
     - frac (float): Fraction of the original dataset to load. Defaults to 1.0, loading the entire dataset.
     - noface (bool): If True, use the noface dataset. Defaults to True.
     - balanced (bool): If True, balance the dataset based on the distribution of sign categories. Defaults to False.
-    - num_signs (int): Number of random sign categories to include. Defaults to NUM_CLASSES.
+    - n_classes (int): Number of random classes to include. Defaults to NUM_CLASSES.
+    - n_frames (int): Maximum number of frames allowed for a row to be included in the filtered DataFrame. Defaults to MAX_SEQ_LEN.
 
     Returns:
     - DataFrame: Subset of the training data according to the specified parameters.
 
     Notes:
-    - If balanced is set to True, the dataset is balanced based on the specified number of sign categories (num_signs).
-    - The balanced dataset will have an equal number of samples for each selected sign category, up to the original distribution.
+    - If balanced is set to True, the dataset is balanced based on the specified number of classes (n_classes).
+    - The balanced dataset will have an equal number of samples for each selected class, up to the original distribution.
     '''
     train = pd.read_csv(TRAIN_CSV_PATH)
-    train=filter_out_parquet_frame(load_frame_number_parquet(train))
 
+    total_size = len(train) # total size
+    size = total_size # current size (will be modified by the filters)
 
-    # replace path train_landmark_files with train_landmark_files_noface
+    # Replace path train_landmark_files with train_landmark_files_noface
     if noface:
         train[['path']] = train[['path']].apply(lambda x: x.str.replace(
             'train_landmark_files', 'train_landmark_files_noface'))
 
     train['file_path'] = TRAIN_DATA_DIR + os.path.sep + train['path']
 
+    # Filter out parquet files with more than n_frames
+    if n_frames is not None:
+        train = load_frame_number_parquet(train) # Add n_frames column
+        train = filter_out_parquet_frame(train, n_frames=n_frames)
+        new_size = len(train)
+        size_ratio = new_size / size
+        print(f"✅ Filtered on n_frames = {n_frames}. Size reduced from {size} to {new_size} ({size_ratio*100:.1f}%)")
+        size = new_size
+
     # Balance the data if requested
     if balanced:
-        if num_signs is not None:
-            # Randomly select num_signs from all available sign categories in the dataset
-            include_signs = random.sample(list(train['sign'].unique()), num_signs)
-        else:
-            include_signs = train['sign'].unique()
-            num_signs = len(include_signs)
-
         # Filter the dataset to include only the selected sign categories
-        train_subset = train[train['sign'].isin(include_signs)]
+        if n_classes is not None:
+            # Randomly select n_classes from all available classes in the dataset
+            include_classes = random.sample(list(train['sign'].unique()), n_classes)
+            train = train[train['sign'].isin(include_classes)]
+            new_size = len(train)
+            size_ratio = new_size / size
+            print(f"✅ Filtered on n_classes = {n_classes}. Size reduced from {size} to {new_size} ({size_ratio*100:.1f}%)")
+            size = new_size
+        else:
+            # include_classes = train['sign'].unique()
+            n_classes = len(include_classes)
 
         # Calculate the target number of samples after balancing
-        initial_size = len(train)
-        target_size = int(initial_size * frac)
-        target_size_per_sign = target_size // num_signs
+        target_size = int(size * frac)
+        target_size_per_class = target_size // n_classes
 
         # Calculate how many samples are remaining after distributing equally among sign categories
-        remaining_samples = target_size % num_signs
+        remaining_samples = target_size % n_classes
 
-        min_size_per_sign = min(train_subset.sign.value_counts()) # min number of elements per sign, in the selected signs
+        min_size_per_class = min(train.sign.value_counts()) # min number of elements per sign, in the selected classes
 
         # If not enough samples, we reduce the sampling size to that value
-        if min_size_per_sign < target_size_per_sign:
-            print(f'Warning: total size smaller than requested, with {min_size_per_sign} per sign instead of {target_size_per_sign}')
-            target_size_per_sign = min_size_per_sign
+        if min_size_per_class < target_size_per_class:
+            print(f'⚠️ Total size smaller than requested, with {min_size_per_class} per sign instead of {target_size_per_class}')
+            target_size_per_class = min_size_per_class
             remaining_samples = 0 # don't add extra samples in this case, we put min_size_per_sign for each sign
 
         # Initiate the data before concatenation
         remaining_samples_added = 0
-        balanced_data = pd.DataFrame()
+        train_balanced = pd.DataFrame()
 
         # For each selected sign category, adjust the number of samples to match the target size
-        for sign_category in include_signs:
-            sign_data = train[train['sign'] == sign_category]
+        for class_ in include_classes:
+            train_class = train[train['sign'] == class_]
 
             if remaining_samples_added < remaining_samples:
-
-                sign_data = sign_data.sample(target_size_per_sign+1)  # add 1 aditional sample to reach the exact total
+                train_class = train_class.sample(target_size_per_class + 1)  # add 1 aditional sample to reach the exact total
                 remaining_samples_added += 1
             else:
-                sign_data = sign_data.sample(target_size_per_sign) # add aditional sample
+                train_class = train_class.sample(target_size_per_class)
 
-            balanced_data = pd.concat([balanced_data, sign_data], ignore_index=True)
+            train_balanced = pd.concat([train_balanced, train_class], ignore_index=True)
 
-        size = len(balanced_data)
-        size_ratio = size / initial_size
-        print(f"Size reduced from {initial_size} to {size} ({size_ratio*100:.1f}%)")
+        size_per_class = len(train_balanced) / n_classes
 
-        return balanced_data.sample(frac=1).reset_index(drop=True) # resample to shuffle
+        new_size = len(train_balanced)
+        size_ratio = new_size / size
+        print(f"✅ Balanced data, with average of {size_per_class} elements per class. Size reduced from {size} to {new_size} ({size_ratio*100:.1f}%)")
 
+        total_size_ratio = new_size / total_size
+        print(f"✅ Loaded {size} rows ({total_size_ratio *100:.1f}% of the original {total_size} rows) from the dataset.")
+        return train_balanced.sample(frac=1).reset_index(drop=True) # resample to shuffle
+
+    # Case if not balanced but n_classes is specified
+    elif n_classes is not None:
+        include_classes = random.sample(list(train['sign'].unique()), n_classes)
+        train = train[train['sign'].isin(include_classes)]
+        train = train.sample(frac=frac)
+
+        new_size = len(train)
+        size_ratio = new_size / size
+        print(f"✅ Filtered on n_classes = {n_classes}. Size reduced from {size} to {new_size} ({size_ratio*100:.1f}%)")
+
+        size = new_size
+        total_size_ratio = size / total_size
+        print(f"✅ Loaded {size} rows ({total_size_ratio *100:.1f}% of the original {total_size} rows) from the dataset.")
+
+        return train.reset_index(drop=True)
+
+    # Case if not balanced and n_classes is not specified
     else:
         train = train.sample(frac=frac)
+
+        new_size = len(train)
+        size_ratio = new_size / size
+        print(f"✅ Loaded {size} rows ({total_size_ratio *100:.1f}% of the original {total_size} rows) from the dataset.")
+
         return train.reset_index(drop=True)
 
 
 def load_frame_number_parquet(train, csv_path=TRAIN_DATA_DIR):
     """
-    Enhances the input 'train' DataFrame by adding a 'frame_parquet' column which calculates the number of frames
-    for each parquet file referenced in the DataFrame. If a CSV file at the specified path ('csv_path') named 'train_frame.csv'
-    already exists, this function loads the DataFrame from that CSV instead of re-processing the parquet files.
+    Enhances the input DataFrame by adding a 'frame_parquet' column indicating the number of frames
+    for each referenced parquet file. If 'train_frame.csv' exists at 'csv_path', loads DataFrame from it.
 
     Parameters:
-    - train (pd.DataFrame): The input DataFrame containing a column 'file_path' with paths to the parquet files.
-    - csv_path (str, optional): The directory path where 'train_frame.csv' will be saved or loaded from.
-      Defaults to 'TRAIN_DATA_DIR'.
+    - train (pd.DataFrame): Input DataFrame with 'file_path' column containing parquet file paths.
+    - csv_path (str, optional): Directory path for 'train_frame.csv'. Defaults to 'TRAIN_DATA_DIR'.
 
     Returns:
-    - pd.DataFrame: The enhanced DataFrame with a 'frame_parquet' column indicating the number of frames
-      for each file. If 'train_frame.csv' exists, returns the DataFrame loaded from this CSV.
+    - pd.DataFrame: Enhanced DataFrame with 'frame_parquet' column. Loads from CSV if exists.
 
     Note:
-    - This function checks for the existence of 'train_frame.csv' in the specified 'csv_path'.
-      If the file does not exist, it calculates the frame difference for each parquet file and saves the result as a CSV.
-      If the file exists, it loads and returns the DataFrame from the CSV, bypassing the calculation.
-    - Make sure 'TRAIN_DATA_DIR' is defined and accessible in your environment before using this function.
-
+    - Checks for 'train_frame.csv' existence. Calculates frames if not. Ensure 'TRAIN_DATA_DIR' is defined.
     """
-    csv_path=TRAIN_DATA_DIR + "/train_frame.csv"
+    csv_path = TRAIN_DATA_DIR + os.path.sep + "train_frame.csv"
 
-
-    # Check if csv file already exist
+    # Check if csv file already exists
     if not os.path.exists(csv_path):
+
+        train_with_frame_count = train.copy()
         # If not existing create the column and save the data frame
-        for i in range(len(train)):
-            df = pd.read_parquet(train.loc[i, "file_path"]).copy()
-            train.at[i, "frame_parquet"] = df["frame"].iloc[-1] - df["frame"].iloc[0] + 1
-        train[["sequence_id","frame_parquet"]].to_csv(csv_path, index=False)
-        print(f" ✅ File has been saved at : {csv_path}")
+        with tqdm(total=len(train), desc="Reading parquet files to count frames") as pbar:
+            for i in range(len(train)):
+                pq_file_path = train.loc[i, "file_path"]
+                parquet_df = pd.read_parquet(pq_file_path )
+                n_frames = parquet_df["frame"].nunique() # calculate the number of frames
+                n_frames2 = parquet_df["frame"].iloc[-1] - parquet_df["frame"].iloc[0] + 1 # calculate the number of frames from start to end
+                if n_frames != n_frames2:
+                    print(f"!! Warning, file {pq_file_path} might have missing frames")
+
+                train_with_frame_count.loc[i, "n_frames"] = n_frames
+                train_with_frame_count.loc[i, "n_frames2"] = n_frames2
+
+                pbar.update(1)
+
+        train_with_frame_count = train_with_frame_count[["sequence_id", "n_frames", "n_frames2"]]
+        train_with_frame_count.to_csv(csv_path, index=False)
+        print(f" ✅ File with frame_parquet has been saved at : {csv_path}")
+
+    # if file exists, load it
     else:
-        full_df = pd.read_csv(csv_path)
-        train = pd.merge(train,full_df,how="left")
-        print("File already exists, loaded matching 'sequence_id' rows.")
+        train_with_frame_count = pd.read_csv(csv_path)
+        print("✅ File with frames already exists, loaded matching 'sequence_id' rows.")
+
+    train = pd.merge(train, train_with_frame_count, how="left", on='sequence_id')
 
     return train
 
 def filter_out_parquet_frame(df, n_frames=MAX_SEQ_LEN):
     """
-    Filters the DataFrame by the 'frame_parquet' column to include only rows where the number of frames is less than or equal to the specified threshold.
+    Filters the DataFrame by the 'n_frames' column to include only rows where the number of frames is less than or equal to the specified threshold.
     This function is intended to be used on DataFrames that have already been processed by the 'load_frame_number_parquet'
-    function, which adds the 'frame_parquet' column indicating the number of frames for each parquet file.
+    function, which adds the 'n_frames' column indicating the number of frames for each parquet file.
 
     Parameters:
-    - df (pd.DataFrame): The DataFrame to filter. It must include a 'frame_parquet' column.
+    - df (pd.DataFrame): The DataFrame to filter. It must include a 'n_frames' column.
     - n_frames (int): The maximum number of frames allowed for a row to be included in the filtered DataFrame.
 
     Returns:
-    - pd.DataFrame: A new DataFrame consisting of rows from the original DataFrame where the 'frame_parquet' value is less than or equal to 'n_frame'.
+    - pd.DataFrame: A new DataFrame consisting of rows from the original DataFrame where the 'n_frames' value is less than or equal to 'n_frame'.
     The index of the DataFrame will be reset.
 
     """
-    return df[df["frame_parquet"] <= n_frames].reset_index(drop=True)
+    return df[df["n_frames"] <= n_frames].reset_index(drop=True)
 
 
 ################################################################################
