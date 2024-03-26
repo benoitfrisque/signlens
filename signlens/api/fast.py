6008c07aeb6d37322eef35362d4eb0167c7e2ecd
Benoit Frisque <benoitfrisque@gmail.com>
2024-03-26 14:34:56 +0100

    cleaned code

--- signlens/api/fast.py
+++ signlens/api/fast.py
@@ -23,15 +23,13 @@ app.add_middleware(
 model_file = "model_v2_250signs.keras"
 root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 model_path = os.path.join(root_dir, 'models_api', model_file)
-model = load_model(mode='from_path', model_path=model_path)
-
-app.state.model = model
+app.state.model = load_model(mode='from_path', model_path=model_path)
 
 # Takes in a JSON file
+
+
 @app.post("/predict_file")
 async def upload_file(file: UploadFile = File(...)):
-
-    model = app.state.model
 
     if not file:
         raise HTTPException(status_code=400, detail="No file provided")
@@ -45,7 +43,7 @@ async def upload_file(file: UploadFile = File(...)):
     data_processed = pad_and_preprocess_sequence(landmarks)
     data_tf = reshape_processed_data_to_tf(data_processed)
 
-    prediction = model.predict(data_tf)
+    prediction = app.state.model.predict(data_tf)
 
     pred, proba = decode_labels(prediction)
 
@@ -54,11 +52,10 @@ async def upload_file(file: UploadFile = File(...)):
 
     return {'sign:': pred, 'probability:': proba}
 
+
 @app.post("/predict")
 async def predict(request: Request):
     data = await request.json()
-
-    model = app.state.model
 
     if not data:
         raise HTTPException(status_code=400, detail="No data provided")
@@ -69,7 +66,7 @@ async def predict(request: Request):
     data_processed = pad_and_preprocess_sequence(landmarks)
     data_tf = reshape_processed_data_to_tf(data_processed)
 
-    prediction = model.predict(data_tf)
+    prediction = app.state.model.predict(data_tf)
 
     word, proba = decode_labels(prediction)
 
@@ -78,10 +75,8 @@ async def predict(request: Request):
 
     return {'Word:': word, 'Probability:': proba}
 
+
 @app.get("/")
 def root():
 
     return {"Welcome to SignLens"}
-
-# Run --> uvicorn signlens.api.fast:app --reload
-# Test JSON file: /Users/max/code/benoitfrisque/signlens/processed_data/07070_landmarks.json
